3a14c15d95ba50a121df7bec40a6d5fc0b381af2
Fernando Piovezan <fernando.piovezan@visionnaire.com.br>
2021-12-03 11:06:43 -0300

    Adicionado query cache e monitor de sistema - QueryCacheable e Telescope

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,19 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Rennokki\QueryCache\Traits\QueryCacheable;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, QueryCacheable;
+
+    /**
+     * Specify the amount of time to cache queries.
+     * Do not specify or set it to null to disable caching.
+     *
+     * @var int|\DateTime
+     */
+    public int|\DateTime $cacheFor = 3600;
 
     /**
      * The attributes that are mass assignable.
